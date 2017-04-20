6258d881f6b794d9a09e9dec414ffb5182cd056f
Martha-PC\Martha <Marth.Brain@gmail.com>
2017-04-20 11:05:47 -0400

    third commit - re structured code - having problems with getting the if/elses to run properly, the answer is always incorrect even when you put in the right answer

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ char ansEig[16];
 char ansNine[16];
 char ansTen[16];
 char agree;
-char corr = 'I';
+int  corr = '10';
 
 main()
 {
@@ -30,67 +30,157 @@ main()
         if (toupper (ansOne) == 'LEFT')
     {
         printf("\nCorrect!\n");
-        printf("Question#2: How many eggs does Gaston eat for breakfast?\n");
-        scanf(" %s", ansTwo);
-        if (toupper (ansTwo) == 'FIVE DOZEN')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#3:In Alice in Wonderland, what is the name of Alice's cat?\n");
-        scanf(" %s", ansThre);
-        if (toupper (ansThre) == 'DINAH')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#4: What is the only Disney animated film where the main character dosen't speak?\n");
-        scanf(" %s", ansFour);
-        if (toupper (ansFour) == 'DUMBO')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#5: What did Jasmine steal from the market?\n");
-        scanf(" %s", ansFiv);
-        if (toupper (ansFiv) == 'APPLE')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#6: What is the original name of Pete's mother figure in Pete's Dragon?\n");
-        scanf(" %s", ansSix);
-        if (toupper (ansSix) == 'NORA')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#7: What is the name of the family who owns Lady in Lady and the Tramp?\n");
-        scanf(" %s", ansSev);
-        if (toupper (ansSev) == 'THE DARLINGS' || (ansSev) == 'DARLING')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#8:What is the name of Maleficent's raven?\n");
-        scanf(" %s", ansEig);
-        if (toupper (ansEig) == 'DIABLO')
-        printf("\nCorrect!\n");
-        printf("Question#9: In the Hunchback of Notre-dame, Who are Quasimodos only friends before Esmeralda?\n");
-        scanf(" %s", ansNine);
-        if (toupper (ansNine) == 'THE GARGOYLES' || (ansNine) == 'GARGOYLES')
-        {
-        printf("\nCorrect!\n");
-        printf("Question#10:Final Question: What is the name of Bambi's Rabbit Friend?\n");
-        scanf(" %s", ansNine);
-        if (toupper (ansNine) == 'THUMPER')
-        {
-            printf("You have finished the quiz!\n")
-        }
-        }
-        }
-        }
-        }
-        }
-        }
-        }
     }
 
     else
     {
-        printf("Incorrect!\n");
-        printf("Would you like to try again (Y/N)?\n");
-        scanf(" %c", &agree);
-
+        printf("\nIncorrect\n");
+        --corr;
     }
+
+
+
+    printf("Question#2: How many dozen eggs does Gaston eat for breakfast?\n");
+    scanf(" %s", ansTwo);
+    if (toupper (ansTwo) == 'FIVE')
+        {
+
+        printf("\nCorrect!\n");
+        }
+
+    else
+    {
+      printf("\nIncorrect.\n");
+      --corr;
+    }
+
+
+
+    printf("Question#3:In Alice in Wonderland, what is the name of Alice's cat?\n");
+    scanf(" %s", ansThre);
+    if (toupper (ansThre) == 'DINAH')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+    printf("Question#4: What is the only Disney animated film where the main character dosen't speak?\n");
+    scanf(" %s", ansFour);
+    if (toupper (ansFour) == 'DUMBO')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#5: What did Jasmine steal from the market?\n");
+    scanf(" %s", ansFiv);
+    if (toupper (ansFiv) == 'APPLE')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#6: What is the original name of Pete's mother figure in Pete's Dragon?\n");
+    scanf(" %s", ansSix);
+    if (toupper (ansSix) == 'NORA')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#7: What is the name of the family who owns Lady in Lady and the Tramp?\n");
+    scanf(" %s", ansSev);
+    if (toupper (ansSev) == 'DARLINGS' || (ansSev) == 'DARLING')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#8:What is the name of Maleficent's raven?\n");
+    scanf(" %s", ansEig);
+    if (toupper (ansEig) == 'DIABLO')
+    {
+        printf("\nCorrect!\n");
+    }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#9: In the Hunchback of Notre-dame, Who are Quasimodos only friends before Esmeralda?\n");
+    scanf(" %s", ansNine);
+    if (toupper (ansNine) == 'GARGOYLES')
+        {
+        printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect.\n");
+        --corr;
+    }
+
+
+
+    printf("Question#10:Final Question: What is the name of Bambi's Rabbit Friend?\n");
+    scanf(" %s", ansNine);
+    if (toupper (ansNine) == 'THUMPER')
+        {
+            printf("\nCorrect!\n");
+        }
+    else
+    {
+        printf("\nIncorrect!\n");
+        --corr;
+    }
+
+
+
+    if (corr == 10)
+    {
+    printf("You have finished the quiz!");
+    printf("You scored %d of 10!", corr);
+    }
+    else
+    {
+    printf("You have finished the quiz!");
+    printf("You scored %d of 10!", corr);
+    }
+
+    printf("Would you like to try again (Y/N)?\n");
+    scanf(" %c", &agree);
+
     }while (toupper (agree) == 'Y');
 
 
